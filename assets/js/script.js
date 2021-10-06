5b1cbbbb88e8be1b5c9076e5225777b4463d73a1
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-06 13:56:43 +0800

    Add all variables to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
 const btnDaily = document.querySelector("#btn-daily")
 const btnWeekly = document.querySelector("#btn-weekly");
 const btnMonthly = document.querySelector("#btn-monthly");
+
+const cardContent = document.querySelectorAll('#card__content');
+let paraCurrent = document.querySelectorAll("#card__time--current");
+let spanPrevious = document.querySelectorAll("#card__time--previous");
