ced77fe81f76b76d98529497b4068de3c9356e01
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-07 10:34:57 +0800

    Add functions which listens for the weekly button click and pushes time for each card

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,14 +38,28 @@ async function requestTimeData() {
   const response = await fetch('data.json');
   const data = await response.json();
 
-  // Listen to the Daily btn click //
+  // Listen to the Daily btn click
   btnDaily.addEventListener("click", event => {
     event.preventDefault();
-    //Iterates through the cards and pushes time data to the cards
+
+    //Iterates through the cards and pushes time data to each card
     cardContent.forEach(element => {
       for (let i = 0; i < data.length; i++) {
         timeCurrent[i].textContent = data[i].timeframes.daily.current + (data[i].timeframes.daily.current > 1 ? "hrs" : "hr");
         timePrevious[i].textContent = "Yesterday - " + data[i].timeframes.daily.previous + (data[i].timeframes.daily.previous > 1 ? "hrs" : "hr");
+      }
+    });
+  });
+
+  // Listen to the Weekly btn click
+  btnWeekly.addEventListener("click", event => {
+    cardContent.forEach(element => {
+      event.preventDefault();
+
+      //Iterates through the cards and pushes time data to each card
+      for (let i = 0; i < data.length; i++) {
+        timeCurrent[i].textContent = data[i].timeframes.weekly.current + "hrs";
+        timePrevious[i].textContent = "Last Week - " + data[i].timeframes.weekly.previous + "hrs";
       }
     });
   });
