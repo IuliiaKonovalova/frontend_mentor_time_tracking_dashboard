ba60f8cc637fa59aa661892762d5e56db4d26e5f
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-07 10:23:01 +0800

    Delete titles in html file and add id for each title

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ const btnWeekly = document.querySelector("#btn-weekly");
 const btnMonthly = document.querySelector("#btn-monthly");
 
 const cardContent = document.querySelectorAll('#card__content');
+let cardName = document.querySelectorAll("#card_name");
 let timeCurrent = document.querySelectorAll("#card__time--current");
 let timePrevious = document.querySelectorAll("#card__time--previous");
 
@@ -11,3 +12,18 @@ document.addEventListener('DOMContentLoaded', function () {
   requestAllData();
   requestTimeData();
 });
+
+/**
+ * Load Cards' content onLoad
+ */
+async function requestAllData() {
+  const response = await fetch('data.json');
+  const data = await response.json();
+  cardContent.forEach(element => {
+    for (let i = 0; i < data.length; i++) {
+      cardName[i].textContent = data[i].title;
+      timeCurrent[i].textContent = data[i].timeframes.daily.current + (data[i].timeframes.daily.current > 1 ? "hrs" : "hr");
+      timePrevious[i].textContent = "Yesterday - " + data[i].timeframes.daily.previous + (data[i].timeframes.daily.previous > 1 ? "hrs" : "hr");
+    }
+  })
+};
