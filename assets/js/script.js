457ca1265411e94f975a3ca5e5057f29f5d032f1
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-07 10:14:03 +0800

    Add function DOMContent on load

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,11 @@ const btnWeekly = document.querySelector("#btn-weekly");
 const btnMonthly = document.querySelector("#btn-monthly");
 
 const cardContent = document.querySelectorAll('#card__content');
-let paraCurrent = document.querySelectorAll("#card__time--current");
-let spanPrevious = document.querySelectorAll("#card__time--previous");
+let timeCurrent = document.querySelectorAll("#card__time--current");
+let timePrevious = document.querySelectorAll("#card__time--previous");
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  requestAllData();
+  requestTimeData();
+});
