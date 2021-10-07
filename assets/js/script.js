1ed016073ac2ab77559eb3b05ed9867eacf58052
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-07 12:51:15 +0800

    Replace ids with classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,10 @@ const btnDaily = document.querySelector("#btn-daily");
 const btnWeekly = document.querySelector("#btn-weekly");
 const btnMonthly = document.querySelector("#btn-monthly");
 
-const cardContent = document.querySelectorAll('#card__content');
-let cardName = document.querySelectorAll("#card_name");
-let timeCurrent = document.querySelectorAll("#card__time--current");
-let timePrevious = document.querySelectorAll("#card__time--previous");
+const cardContent = document.querySelectorAll('.card__content');
+let cardName = document.querySelectorAll(".card_name");
+let timeCurrent = document.querySelectorAll(".card__time--current");
+let timePrevious = document.querySelectorAll(".card__time--previous");
 
 /**
  * Load Cards' content onLoad
