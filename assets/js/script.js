eb6470e07ee4fcdc1ece7a67c02289f0717d46c4
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-07 10:31:25 +0800

    Add functions which listens for the daily button click and pushes time for each card

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,3 +28,26 @@ async function requestAllData() {
     }
   })
 };
+
+
+/**
+ * Make request to json file
+ * Get data from json file
+ */
+async function requestTimeData() {
+  const response = await fetch('data.json');
+  const data = await response.json();
+
+  // Listen to the Daily btn click //
+  btnDaily.addEventListener("click", event => {
+    event.preventDefault();
+    //Iterates through the cards and pushes time data to the cards
+    cardContent.forEach(element => {
+      for (let i = 0; i < data.length; i++) {
+        timeCurrent[i].textContent = data[i].timeframes.daily.current + (data[i].timeframes.daily.current > 1 ? "hrs" : "hr");
+        timePrevious[i].textContent = "Yesterday - " + data[i].timeframes.daily.previous + (data[i].timeframes.daily.previous > 1 ? "hrs" : "hr");
+      }
+    });
+  });
+
+}
